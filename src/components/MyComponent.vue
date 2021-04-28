6e759fd5ad63bbb10717a4e2ef3cafb2ec370813
Jerry Johansson <jerryjohansson@MBPsomthorJerry.lan>
2021-04-28 10:15:04 +0200

    Nya uppdateringar

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Jerry Johansson - Mountain Flame</h1>
+    <img src="../assets/mountainflame-500px.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+name: "MyComponent"
+}
+</script>
+
+<style scoped>
+h1{
+  color: red;
+}
+</style>
